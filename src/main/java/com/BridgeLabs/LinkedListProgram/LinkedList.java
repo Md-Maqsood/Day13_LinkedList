d076e5c2f8f8a4499e4a8caa1181947a84639099
maqsood <maqsoodhamd37@gmail.com>
2020-10-13 19:25:36 +0530

    [Maqsood].add: initial commit

---
+++ src/main/java/com/BridgeLabs/LinkedListProgram/LinkedList.java
@@ -0,0 +1,11 @@
+package com.BridgeLabs.LinkedListProgram;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LinkedList {
+	private static final Logger logger=LogManager.getLogger(LinkedList.class);
+	public static void main(String[] args) {
+		logger.debug("Welcome to linkedlist, stack and Queue program");
+	}
+}
